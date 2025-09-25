e1f9aebde834c38cf19a8f911060da6d680f36df
김재범 <beomzh@opennaru.com>
2025-09-25 13:15:57 +0900

    JDK8 버전 추가

---
+++ my-legacy-app/src/main/java/com/openmaru/my_legacy_app/MyLegacyAppApplication.java
@@ -0,0 +1,13 @@
+package com.openmaru.my_legacy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLegacyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLegacyAppApplication.class, args);
+	}
+
+}
---
+++ my-legacy-app/src/test/java/com/openmaru/my_legacy_app/MyLegacyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmaru.my_legacy_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLegacyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
