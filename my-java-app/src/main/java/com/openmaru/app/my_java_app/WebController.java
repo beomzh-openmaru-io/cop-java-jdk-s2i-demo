d087ec273f93173e2d422ff1f86096deba80059c
김재범 <beomzh@opennaru.com>
2025-09-25 10:08:06 +0900

    first commit

---
+++ my-java-app/src/main/java/com/openmaru/app/my_java_app/WebController.java
@@ -0,0 +1,13 @@
+package com.openmaru.app.my_java_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ my-java-app/src/test/java/com/openmaru/app/my_java_app/MyJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmaru.app.my_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
