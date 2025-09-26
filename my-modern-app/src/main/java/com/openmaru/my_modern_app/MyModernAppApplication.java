2e2bdaefef5c904d4d7f598ffc3e3bc08aa6663d
김재범 <beomzh@opennaru.com>
2025-09-26 10:46:27 +0900

    jdk21 추가

---
+++ my-modern-app/src/main/java/com/openmaru/my_modern_app/MyModernAppApplication.java
@@ -0,0 +1,13 @@
+package com.openmaru.my_modern_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyModernAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyModernAppApplication.class, args);
+	}
+
+}
---
+++ my-modern-app/src/test/java/com/openmaru/my_modern_app/MyModernAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmaru.my_modern_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyModernAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
